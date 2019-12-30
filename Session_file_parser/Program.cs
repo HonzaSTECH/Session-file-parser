18455408ef5ccf800b0a701cebd0cff3dd1c0b8d
Jan Štěch <honza.stech@gmail.com>
2019-12-30 22:55:47 +0100

    The Programme now reads input and processes data, except for arrays.
    Variables are saved as objects, but no result is outputted yet.

--- Session_file_parser/Program.cs
+++ Session_file_parser/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -73,11 +73,12 @@ namespace Session_file_parser
             get { return value; }
             set { this.value = value; }
         }
-        public StringVariable(string name, char type, string value)
+        public StringVariable(string name, char type, int length, string value)
         {
             if (type != 's') { throw new InvalidDataException(); }
             this.Name = name;
             this.Type = type;
+            this.Length = length;
             this.Value = value;
         }
     }
@@ -91,7 +92,7 @@ namespace Session_file_parser
         }
         public BooleanVariable(string name, char type, bool value)
         {
-            if (type != 'b') { throw new InvalidDataException(); }if (type != 'i' && type != 'd') { throw new InvalidDataException(); }
+            if (type != 'b') { throw new InvalidDataException(); }
             this.Name = name;
             this.Type = type;
             this.Value = value;
@@ -129,6 +130,12 @@ namespace Session_file_parser
 
     class Program
     {
+        const char nameDelimiter = '|';
+        const char valueDelimiter = ':';
+        const char variableDelimiter = ';';
+        const char stringSigns = '"';
+        const char arrayEndSign = '}';
+
         static void Main(string[] args)
         {
             Console.WriteLine("Do you want to provide input as a file or as a text? (F/T)");
@@ -181,12 +188,110 @@ namespace Session_file_parser
             Console.WriteLine("Press any key to start parsing of your input");
             Console.ReadKey(true);
 
-            string result = String.Empty;
-            
-            /*
-             TODO Program
-             */
+            if (inputType == 'f')
+            {
+                input = File.ReadAllText(inputPath);
+            }
 
+            int i = 0;
+            int varCount = 0;
+            int inputLength = input.Length;
+            string result = String.Empty;
+            Dictionary<int, Variable> vars = new Dictionary<int, Variable>();
+
+            while (i < inputLength)
+            {
+                varCount++;
+                Console.WriteLine("Reading variable #{0}", varCount);
+
+                //Getting the name of the variable
+                string var_name = String.Empty;
+                while (input[i] != nameDelimiter)
+                {
+                    var_name += input[i];
+                    i++;
+                }
+                i++;    //Skipping the delimiter
+                Console.WriteLine("\tName: {0}", var_name);
+
+                //Getting the type of the variable
+                char var_type = input[i];
+                i += 2;    //Skipping the type and the delimiter
+                Console.WriteLine("\tType: {0}", var_type.ToString());
+
+                //Getting the length if array or string
+                string var_length_str = String.Empty;
+                int var_length = 0;
+                if (var_type == 'a' || var_type == 's')
+                {
+                    while (input[i] != valueDelimiter)
+                    {
+                        var_length_str += input[i];
+                        i++;
+                    }
+                    var_length = Convert.ToInt32(var_length_str);
+                    i++;    //Skipping the delimiter
+                    Console.WriteLine("Length: {0}", var_length);
+                }
+
+                //Getting the value of the variable
+                string var_value_str = String.Empty;
+                switch (var_type)   //Getting the raw value
+                {
+                    case 'i':   //Integer   \
+                    case 'd':   //Double     --> Ending with ;
+                    case 'b':   //Boolean   /
+                        while (input[i] != variableDelimiter)
+                        {
+                            var_value_str += input[i];
+                            i++;
+                        }
+                        i++;    //Skipping the delimiter
+                        break;
+                    case 's':   //String    --> Surronded with " "
+                        i++;    //Skipping the starting "
+                        while (input[i] != stringSigns)
+                        {
+                            var_value_str += input[i];
+                            i++;
+                        }
+                        i += 2;    //Skipping the ending " and the delimiter
+                        break;
+                    case 'a':   //Array     --> Surronded with { }
+                        i++;    //Skipping the {
+                        while (input[i] != arrayEndSign)
+                        {
+                            var_value_str += input[i];
+                            i++;
+                        }
+                        i++;    //Skipping the }
+                        break;
+                }
+                switch (var_type)   //Parsing the raw value and saving into object
+                {
+                    case 'i':   //Integer
+                        int var_value_i = Convert.ToInt32(var_value_str);
+                        vars.Add(varCount, new IntegerVariable(var_name, var_type, var_value_i));
+                        break;
+                    case 'd':   //Double
+                        double var_value_d = double.Parse(var_value_str, System.Globalization.CultureInfo.InvariantCulture);
+                        vars.Add(varCount, new DecimalVariable(var_name, var_type, var_value_d));
+                        break;
+                    case 'b':   //Boolean
+                        bool var_value_b = (var_value_str == "1") ? true : false;
+                        vars.Add(varCount, new BooleanVariable(var_name, var_type, var_value_b));
+                        break;
+                    case 's':   //String
+                        string var_value_s = var_value_str;
+                        vars.Add(varCount, new StringVariable(var_name, var_type, var_length, var_value_s));
+                        break;
+                    case 'a':   //Array
+                        //TODO
+                        break;
+                }
+
+            }
+            
             Console.WriteLine("Done!");
 
             if (outputType == 't')
